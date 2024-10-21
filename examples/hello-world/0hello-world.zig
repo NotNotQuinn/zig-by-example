6a0be679914498c59179ff9963123c272976ab60
Quinn D.T. <git@quinndt.ca>
2024-10-21 00:15:35 -0500

    Improve hello-world example

--- examples/hello-world/0hello-world.zig
+++ examples/hello-world/0hello-world.zig
@@ -1,6 +1,8 @@
-// This is a test
+// The first example is the standard 'Hello, World!'
+// Here is the full source code:
+
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("Hello, World!", .{});
+pub fn main() void {
+    std.debug.print("Hello, World!\n", .{});
 }
--- examples/hello-world/1hello-world.sh
+++ examples/hello-world/1hello-world.sh
@@ -1,6 +1,18 @@
 # To run the program, put the code in `hello-world.zig`
-# and use `zig run`.
-$ zig run hello-world.zig
+# and use `zig build-exe` to compile. Then run the
+# generated binary.
+$ zig build-exe hello-world.zig
+$ ls
+hello-world  hello-world.o  hello-world.zig
+$ ./hello-world
 Hello, World!
 
-# WIP
+# Or you can use `zig run` to compile and run.
+$ zig run hello-world.zig
+Hello, World!
+$ ls
+hello-world.zig
+
+# The zig version used to make this website is `0.13.0`.
+$ zig version
+0.13.0
